3757e8dc48a0660dd4840af68245e544899d113d
Max Sveshnikov <msveshnikov@gmail.com>
2022-12-30 18:08:44 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,84 @@
+import fetch from "node-fetch";
+import fs from "node:fs";
+import { Configuration, OpenAIApi } from "openai";
+import TelegramBot from "node-telegram-bot-api";
+process.env["NTBA_FIX_350"] = 1;
+
+const engineId = "stable-diffusion-512-v2-1";
+const apiHost = "https://api.stability.ai";
+const stabilityKey = process.env.STABILITY_KEY;
+if (!stabilityKey) throw new Error("Missing Stability API key.");
+const url = `${apiHost}/v1alpha/generation/${engineId}/text-to-image`;
+const outputFile = "./text_to_image.png";
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
+const openai = new OpenAIApi(configuration);
+const gptResponse = async (prompt) => {
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 1000,
+            temperature: 0.8,
+        });
+        console.log(completion.data.choices[0].text);
+        return completion.data.choices[0].text;
+    } catch (error) {
+        return "Ошибка, сорян";
+    }
+};
+
+const bot = new TelegramBot(process.env.TELEGRAM_KEY, { polling: true });
+
+bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    console.log(msg.text);
+    if (msg.text.startsWith("Нарисуй") || msg.text.startsWith("Draw") || msg.text.startsWith("Paint")) {
+        const prompt = await gptResponse("Переведи на английский:" + msg.text);
+        draw(
+            prompt +
+                " deep focus, highly detailed, digital painting, artstation, smooth, sharp focus, illustration, art by magali villeneuve, ryan yee, rk post, clint cearley, daniel ljunggren, zoltan boros, gabor szikszai, howard lyon, steve argyle, winona nelson"
+        );
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+        bot.sendPhoto(chatId, outputFile);
+        return;
+    }
+    bot.sendMessage(chatId, await gptResponse(msg.text + "."));
+});
+
+async function draw(text) {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "image/png",
+                Authorization: stabilityKey,
+            },
+            body: JSON.stringify({
+                cfg_scale: 7,
+                clip_guidance_preset: "FAST_BLUE",
+                height: 512,
+                width: 512,
+                samples: 1,
+                steps: 20,
+                text_prompts: [
+                    {
+                        text: text,
+                        weight: 1,
+                    },
+                ],
+            }),
+        });
+
+        if (!response.ok) {
+            console.error(`Non-200 response: ${await response.text()}`);
+            return;
+        }
+
+        const writeStream = fs.createWriteStream(outputFile);
+        response.body?.pipe(writeStream);
+    } catch (e) {
+        console.error(e);
+    }
+}
