3a7cd4a426eee70f7213f0d4cc45de0830574260
Max Sveshnikov <msveshnikov@gmail.com>
2023-01-24 09:00:10 +0100

    Minor

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,8 @@
 scp C:\My-progs\Node.JS\chatgpt\index.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 scp C:\My-progs\Node.JS\chatgpt\io.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 scp C:\My-progs\Node.JS\chatgpt\search.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp C:\My-progs\Node.JS\chatgpt\package.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp C:\My-progs\Node.JS\chatgpt\package-lock.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 scp C:\My-progs\Node.JS\chatgpt\.env ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 
 ssh -l ubuntu mega.maxsoft.tk "pm2 restart chatgpt"
