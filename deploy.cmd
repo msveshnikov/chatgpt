aa47b332ca5341e984174de7928c0f1c70ec36c9
Max Sveshnikov <msveshnikov@gmail.com>
2023-02-19 11:40:02 +0100

    Minor

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,10 @@
-scp C:\My-progs\Node.JS\chatgpt\index.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
-scp C:\My-progs\Node.JS\chatgpt\db.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
-scp C:\My-progs\Node.JS\chatgpt\search.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
-scp C:\My-progs\Node.JS\chatgpt\package.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
-scp C:\My-progs\Node.JS\chatgpt\package-lock.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
-scp C:\My-progs\Node.JS\chatgpt\.env ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
-scp C:\My-progs\Node.JS\chatgpt\google.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\index.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\db.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\search.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\package.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\package-lock.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\.env ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\google.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 
 ssh -l ubuntu mega.maxsoft.tk "cd chatgpt && npm install"
 ssh -l ubuntu mega.maxsoft.tk "pm2 restart chatgpt"
