e433e9736e6ba4445bcd5a5f06edcd6206e13392
Max Sveshnikov <msveshnikov@gmail.com>
2023-03-01 21:27:42 +0100

    Minor

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,5 @@
 scp .\index.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
+scp .\ecosystem.config.cjs ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 scp .\db.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 scp .\search.js ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
 scp .\package.json ubuntu@mega.maxsoft.tk:/home/ubuntu/chatgpt/ 
