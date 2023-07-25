baee2e44f7ed0de362cf6184a209e8985d0e4f1e
Max Sveshnikov <msveshnikov@gmail.com>
2023-07-25 21:42:32 +0200

    Minor

--- send.js
+++ send.js
@@ -16,10 +16,27 @@ setInterval(() => {
     console.log(chatId);
     bot.sendMessage(
         chatId,
-        `Привет от Манга ТВ!
+        `📣 Attention manga lovers! 🎉
 
-        Последние истории о Пригожине уже на Манге! 😊
-        Так что не ждите, заходите на https://MangaTV.shop/landing/index.html и наслаждайтесь комиксами с искусственным интеллектом!`
+        Looking for a platform to unleash your creativity and share your captivating manga stories with the world? Look no further! Introducing Manga TV, the ultimate destination for manga enthusiasts like you.
+        
+        🖌️ Create Unique Manga Stories: Express your imagination and bring your stories to life with our intuitive and user-friendly editor. Craft visually stunning panels, add captivating dialogues, and explore various art styles to make your manga truly stand out.
+        
+        🌍 Connect with a Vibrant Community: Join a thriving community of manga artists, writers, and fans from around the globe. Collaborate, share feedback, and learn from fellow creators who share your passion for this captivating art form.
+        
+        📚 Explore a Diverse Collection: Dive into a treasure trove of manga stories created by talented individuals. Discover new genres, art styles, and narratives that will keep you engaged for hours on end.
+        
+        💫 Showcase Your Talent: Publish your manga to our global audience and gain recognition for your creative work. Whether you're a seasoned artist or just starting, Manga TV is the perfect platform to showcase your talent and receive valuable feedback.
+        
+        🔥 Join Manga TV today and unleash your manga creativity like never before! Sign up now at [website URL] and embark on an exciting journey filled with endless possibilities.
+        
+        💌 Have questions or need assistance? Our dedicated support team is here to help. Feel free to reach out to us anytime at [support email].
+        
+        Don't miss out on this incredible opportunity to be part of the Manga TV community. Start creating, sharing, and connecting with like-minded manga enthusiasts today!
+        
+        Spread the word and invite your friends who share a passion for manga. Together, let's take the manga world by storm! 🌟
+        
+        https://MangaTV.shop/landing/index.html`
     )
         .then(() => {})
         .catch((e) => {
