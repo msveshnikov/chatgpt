937992fdefdf85bc43076fc6458ad223065c8e28
Max Sveshnikov <msveshnikov@gmail.com>
2023-08-19 14:11:16 +0200

    bump

--- send.js
+++ send.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(process.env.TELEGRAM_KEY);
 const trial = readTrial();
 const opened = readOpened();
 
-const users = Object.keys(trial)//.filter((chatId) => !opened[chatId]);
+const users = Object.keys(trial).filter((chatId) => !opened[chatId]);
 // every 100 milliseconds pop one element from trial
 console.log(users.length);
 setInterval(() => {
@@ -16,27 +16,17 @@ setInterval(() => {
     console.log(chatId);
     bot.sendMessage(
         chatId,
-        `📣 Attention manga lovers! 🎉
+        `📣 Exciting News! The highly anticipated SDXL 1.0 is finally here, exclusively released on Manga TV! 🎉🎉
 
-        Looking for a platform to unleash your creativity and share your captivating manga stories with the world? Look no further! Introducing Manga TV, the ultimate destination for manga enthusiasts like you.
+        Are you ready to immerse yourself in a world of captivating stories and stunning visuals? Look no further than SDXL 1.0, powered by the cutting-edge GPT-4 AI comics generator from mangatv.shop. With just one prompt, prepare to witness an entire video story unfold before your eyes!
         
-        🖌️ Create Unique Manga Stories: Express your imagination and bring your stories to life with our intuitive and user-friendly editor. Craft visually stunning panels, add captivating dialogues, and explore various art styles to make your manga truly stand out.
+        Experience the next level of storytelling as SDXL 1.0 takes you on thrilling adventures, heartwarming tales, and epic battles. Each panel is meticulously crafted, ensuring the highest quality and attention to detail. You'll be amazed by the lifelike characters, intricate backgrounds, and dynamic action sequences that will leave you on the edge of your seat.
         
-        🌍 Connect with a Vibrant Community: Join a thriving community of manga artists, writers, and fans from around the globe. Collaborate, share feedback, and learn from fellow creators who share your passion for this captivating art form.
+        Whether you're a long-time manga fan or new to the world of comics, SDXL 1.0 guarantees an unforgettable experience. Dive into a vast library of genres, including fantasy, sci-fi, romance, and more. Let your imagination run wild as you explore diverse worlds and meet fascinating characters brought to life by the incredible GPT-4 AI technology.
         
-        📚 Explore a Diverse Collection: Dive into a treasure trove of manga stories created by talented individuals. Discover new genres, art styles, and narratives that will keep you engaged for hours on end.
+        With Manga TV's user-friendly interface, accessing SDXL 1.0 has never been easier. Simply visit mangatv.shop and prepare to be transported into a realm of unlimited possibilities. Get ready to lose yourself in the magic of storytelling like never before.
         
-        💫 Showcase Your Talent: Publish your manga to our global audience and gain recognition for your creative work. Whether you're a seasoned artist or just starting, Manga TV is the perfect platform to showcase your talent and receive valuable feedback.
-        
-        🔥 Join Manga TV today and unleash your manga creativity like never before! Sign up now at [website URL] and embark on an exciting journey filled with endless possibilities.
-        
-        💌 Have questions or need assistance? Our dedicated support team is here to help. Feel free to reach out to us anytime at [support email].
-        
-        Don't miss out on this incredible opportunity to be part of the Manga TV community. Start creating, sharing, and connecting with like-minded manga enthusiasts today!
-        
-        Spread the word and invite your friends who share a passion for manga. Together, let's take the manga world by storm! 🌟
-        
-        https://MangaTV.shop/landing/index.html`
+        Don't miss out on this groundbreaking release! Visit mangatv.shop now and witness the exceptional quality of SDXL 1.0. Get ready to embark on an extraordinary journey that will leave you craving for more. 🌟`
     )
         .then(() => {})
         .catch((e) => {
