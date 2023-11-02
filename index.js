a383cd0036bb8c2aeb04e02cc02ffe159de01e72
Max Sveshnikov <msveshnikov@gmail.com>
2023-11-02 08:51:10 +0100

    SDXL 1.0

--- index.js
+++ index.js
@@ -519,29 +519,32 @@ const getText = async (prompt, temperature, max_tokens, chatId) => {
     }
 };
 
-const getArt = async (prompt) => {
-    const response = await fetch("https://api.stability.ai/v1/generation/stable-diffusion-512-v2-1/text-to-image", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "image/png",
-            Authorization: process.env.STABILITY_KEY,
-        },
-        body: JSON.stringify({
-            cfg_scale: 7,
-            clip_guidance_preset: "FAST_BLUE",
-            height: 512,
-            width: 512,
-            samples: 1,
-            steps: 30,
-            text_prompts: [
-                {
-                    text: prompt,
-                    weight: 1,
-                },
-            ],
-        }),
-    });
+const getArt = async (prompt, xl) => {
+    const response = await fetch(
+        `https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "image/png",
+                Authorization: process.env.STABILITY_KEY,
+            },
+            body: JSON.stringify({
+                cfg_scale: 7,
+                clip_guidance_preset: "FAST_BLUE",
+                height: 1024,
+                width: 1024,
+                samples: 1,
+                steps: 20,
+                text_prompts: [
+                    {
+                        text: prompt,
+                        weight: 1,
+                    },
+                ],
+            }),
+        }
+    );
 
     if (!response.ok) {
         console.error(`Stability AI error: ${(await response.text())?.split("\n")?.[0]?.substring(0, 200)}`);
