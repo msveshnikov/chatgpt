0dd79cf34acbcd7cea3564d9cc1f682ea53fad38
Max Sveshnikov <msveshnikov@gmail.com>
2024-03-16 09:02:36 +0100

    Minor

--- send.js
+++ send.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(process.env.TELEGRAM_KEY);
 const trial = readTrial();
 const opened = readOpened();
 
-const users = Object.keys(trial); //.filter((chatId) => !opened[chatId]);
+const users = Object.keys(trial).filter((chatId) => !opened[chatId]);
 // every 100 milliseconds pop one element from trial
 console.log(users.length);
 setInterval(() => {
@@ -16,24 +16,20 @@ setInterval(() => {
     console.log(chatId);
     bot.sendMessage(
         chatId,
-        `🌟 Unveiling the Latest Manga Marvels at Manga TV! 🌈
+        `🎉 Exciting News! 🎉
 
-        Discover the latest updates that will revolutionize your manga experience:
-
-        1️⃣ Own Video Clip Upload:
-        Now, it's your turn to shine! Bring your stories to life with personalized video clips. Upload your creations directly to Manga TV and captivate your audience with a personal touch.
-
-        2️⃣ Stable AI Video Diffusion:
-        Experience seamless transitions as images effortlessly transform into 4-second clips. Our latest update ensures dynamic content delivery, guaranteeing an uninterrupted viewing experience for your readers.
-
-        3️⃣ Video Effects (Instagram-like Filters):
-        Elevate your videos with Instagram-like filters! Explore a variety of effects to add style and flair to your content, making your manga stories even more engaging and visually stunning.
-
-        ✨ Plus, we've added Webtoons to Manga TV for smoother navigation and enhanced user experience! ✨
-
-        Don't miss out on these exciting additions from last week. Stay tuned for more updates and keep exploring Manga TV for an unforgettable manga journey! 🌟📺 #MangaTV #WhatsNew 
-        Happy Manga-Making, Super Creators! 🚀
-        https://mangatv.shop  
+        🌟 The Trial Period is BACK on ChatGPT bot! 🌟
+        🚀 Gemini Pro 1.5 connected! Try it now ❤️
+        
+        💬 Plus, NEW features added to Mnaga TV recently:
+        
+        ✨ Comments on Stories
+        🎨 Google Studio voices
+        🎙️ Collaboration with ElevenLabs and Own Voices
+        🔊 Voice Manager for Silver+ Users
+        Don't miss out! Start your manga journey today on Manga TV! 🚀
+        
+        #MangaTV #FreeTrial #MangaExperience 📖✨
         `
     )
         .then(() => {})
